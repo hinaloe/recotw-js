7bfc3eb684126f458f980082a5c4083dd516c7ea
hina <hina@hinaloe.net>
2015-02-28 23:49:53 +0900

    add standalone option

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 ./node_modules/.bin/schema-tsifier kitchen/schema.yml RecoTw --with-resources > kitchen/recotw.ts
 ./node_modules/.bin/tsc --target es5 -d kitchen/recotw.ts --module commonjs
-./node_modules/.bin/browserify -t coffeeify --extension=".coffee" src/index.coffee -o dist/recotw.js
+./node_modules/.bin/browserify -t coffeeify --extension=".coffee" src/index.coffee -o dist/recotw.js --standalone="recotw"
